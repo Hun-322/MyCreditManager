4acb0bd52b9c0abe8fe9d43c7fb15afd990d92be
김세훈 <ksh26271@naver.com>
2023-04-29 01:20:09 +0900

    [추가] 성적삭제 로직 구현

--- MyCreditManager/MyCreditManager/Menu/Features/DeleteScore.swift
+++ MyCreditManager/MyCreditManager/Menu/Features/DeleteScore.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 func deleteScore() {
+    print("\(deleteScoreString.안내1)\n\(deleteScoreString.안내2)")
     
+    guard let inputArr = readLine()?.split(separator: " ").map({String($0)}), inputArr.count == 2 else {
+        print(error.입력오류)
+        return
+    }
+    
+    let name = inputArr[0], subjectName = inputArr[1]
+    
+    if let index = arr.firstIndex(where: {$0.name == name}) {
+        arr[index].subject.removeValue(forKey: subjectName)
+        print("\(name) 학생의 \(subjectName) 과목의 성적이 삭제되었습니다.")
+    }else {
+        print("\(name) \(deleteScoreString.삭제실패)")
+    }
 }
