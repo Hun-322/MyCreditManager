ba4866a41edd50652960b80c819a87127450a8f9
김세훈 <ksh26271@naver.com>
2023-04-28 18:50:53 +0900

    [수정] 파일 구조 수정

--- MyCreditManager/MyCreditManager/CodingManager/MenuType.swift
+++ MyCreditManager/MyCreditManager/CodingManager/MenuType.swift
@@ -14,17 +14,5 @@ enum Menu: String {
     case 성적추가 = "3"
     case 성적삭제 = "4"
     case 평점보기 = "5"
-    case 종료 = "6"
-    
-    static func rowValueByString(rawValue: String) {
-        switch rawValue {
-        case 학생추가.rawValue: addStudent()
-        case 학생삭제.rawValue: deleteScore()
-        case 성적추가.rawValue: addScore()
-        case 성적삭제.rawValue: deleteStudent()
-        case 평점보기.rawValue: scoreView()
-        case 종료.rawValue: endMenu()
-        default: print(startString.논외입력메시지)
-        }
-    }
+    case 종료 = "X"
 }
--- MyCreditManager/MyCreditManager/Menu/MenuManager.swift
+++ MyCreditManager/MyCreditManager/Menu/MenuManager.swift
@@ -13,6 +13,15 @@ func menuManager() {
         print(startString.메뉴)
         
         guard let input = readLine() else { return }
-        Menu.rowValueByString(rawValue: input)
+        
+        switch input {
+        case Menu.학생추가.rawValue: addStudent()
+        case Menu.학생삭제.rawValue: deleteStudent()
+        case Menu.성적추가.rawValue: addScore()
+        case Menu.성적삭제.rawValue: deleteScore()
+        case Menu.평점보기.rawValue: scoreView()
+        case Menu.종료.rawValue: endMenu()
+        default: print(startString.논외입력메시지)
+        }
     }
 }
