fbad774975042424ca582b0159d00dd4d19540de
김세훈 <ksh26271@naver.com>
2023-04-28 21:54:10 +0900

    [추가] Subject Model 추가

---
+++ MyCreditManager/MyCreditManager/Models/Subject.swift
@@ -0,0 +1,18 @@
+//
+//  Subject.swift
+//  MyCreditManager
+//
+//  Created by KSH on 2023/04/28.
+//
+
+import Foundation
+
+struct Subject {
+    let name: String
+    var score: Double
+    
+    init(name: String, score: Double) {
+        self.name = name
+        self.score = score
+    }
+}
