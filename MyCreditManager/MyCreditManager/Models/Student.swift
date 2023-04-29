fb709e700d235f9d9baae855ed28dee53a57f428
김세훈 <ksh26271@naver.com>
2023-04-29 17:23:51 +0900

    [수정] 평점 최대 소수점 2자리까지 출력 되도록 수정

--- MyCreditManager/MyCreditManager/Models/Student.swift
+++ MyCreditManager/MyCreditManager/Models/Student.swift
@@ -10,10 +10,11 @@ import Foundation
 struct Student {
     let name: String
     var subject: [String: Score.RawValue] = [:]
-    var avg: Float {
+    var avg: String {
         let totalScore = subject.values.reduce(0.0) { $0 + Score.castIntScore($1) }
         let count = Float(subject.count)
-        return totalScore / count
+        let avg = String(format: "%.2f", totalScore / count)
+        return avg
     }
     
     init(name: String) {
